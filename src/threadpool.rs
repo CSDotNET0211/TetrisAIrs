f6e170b287ba89567a721e6280d114cebe3ee980
CSDotNET <mrapplefruit0211@gmail.com>
2022-07-02 12:38:03 +0900

    探索実装中

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,27 +1,38 @@
 //! cargo docテスト
 //! # aiueo
 
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
         let mut workers = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(receiver));
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn excute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -31,8 +42,13 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            (*job)();
+        }
+        let thread = thread::spawn(|| receiver);
 
         Worker { id, thread }
     }
